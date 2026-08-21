30042147070ca8ae1b7d65d33bf2440b6229a2b8
Peter S <petersundress@gmail.com>
2026-08-21 06:11:22 +0300

    Initial commit

---
+++ logistic/views.py
@@ -0,0 +1,21 @@
+
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Product, Stock
+from .serializers import ProductSerializer, StockSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['positions__product']
+    search_param = 'products'
